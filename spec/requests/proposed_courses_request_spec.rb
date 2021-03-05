9e1e2a10675b481b3f529910c6bb1bd2a0652bdc
Antonio J Rossi <antoniojrossi@gmail.com>
2021-03-05 22:39:19 +0100

    Add aditional test for proposed courses creation

--- spec/requests/proposed_courses_request_spec.rb
+++ spec/requests/proposed_courses_request_spec.rb
@@ -23,6 +23,13 @@ RSpec.describe 'ProposedCourses', type: :request do
       it 'returns status code 201' do
         expect(response).to have_http_status(201)
       end
+
+      it 'creates a proposed course' do
+        expect(json).not_to be_empty
+        expect(json).to have_key('data')
+        expect(json['data']['relationships']['course']['data']['id']).to eq(course.id.to_s)
+        expect(json['data']['relationships']['teacher']['data']['id']).to eq(teacher.id.to_s)
+      end
     end
   end
 end
